6b74ff42d5fbd74708d84794f830d95515da9a97
CharlesBtto <charles.btto001@gmail.com>
2024-05-22 20:42:52 +0200

    up
    date

--- mon_app.py
+++ mon_app.py
@@ -1,8 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, send_file
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-import os
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
@@ -10,23 +6,45 @@ app.secret_key = 'secret_key'
 
 @app.route('/')
 def exe():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route('/index2', methods=['GET', 'POST'])
 def image():
-  if request.method == 'POST':
-    session['image_url'] = request.form['image_url']
-    image_url = session['image_url']
-  return render_template("index2.html", image_url=image_url)
+    if request.method == 'POST':
+        image_url = request.form.get('image_url', '').strip()
+        if image_url:
+            session['image_url'] = image_url
+            return redirect(url_for('texte'))
+    return render_template("index.html", error="Please provide a valid image URL")
+
+
+@app.route('/index2', methods=['GET'])
+def show_image():
+    image_url = session.get('image_url', None)
+    if image_url:
+        return render_template("index2.html", image_url=image_url)
+    return redirect(url_for('exe'))
 
 
 @app.route('/index3', methods=['GET', 'POST'])
 def texte():
-  if request.method == 'POST':
-    texte_saisi = request.form['texte_saisi']
+    if request.method == 'POST':
+        texte_saisi = request.form.get('texte_saisi', '').strip()
+        if texte_saisi:
+            session['texte_saisi'] = texte_saisi
+            return redirect(url_for('show_meme'))
+    return render_template("index2.html", error="Please provide a valid text description")
+
+
+@app.route('/index3', methods=['GET'])
+def show_meme():
     image_url = session.get('image_url', None)
-  return render_template('index3.html', texte_saisi=texte_saisi, image_url=image_url)
+    texte_saisi = session.get('texte_saisi', None)
+    if image_url and texte_saisi:
+        return render_template('index3.html', texte_saisi=texte_saisi, image_url=image_url)
+    return redirect(url_for('exe'))
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
